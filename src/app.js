a4e846225c4f2f849ccd8dfe2d53efad399fa9c5
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-12 01:19:34 +0000

    Se realiza la organización de las cartas y se termina el proyecto

--- src/app.js
+++ src/app.js
@@ -5,19 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// window.onload = function() {
-//   //write your code here
-//   console.log("Hello Rigo from the console!");
-// };
-
 const inputNumber = document.getElementById("inputNumber");
 const buttonDraw = document.getElementById("buttonDraw");
 const buttonSort = document.getElementById("buttonSort");
-let box = document.getElementById("box");
-//const card = document.getElementById("card");
-const headerSymbol = document.getElementById("symbol-header");
-const bodyNumber = document.getElementById("number-body");
-const footerSymbol = document.getElementById("symbol-footer");
+const box = document.getElementById("box");
 
 let symbols = ["♥", "♠", "♣", "♦"];
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
@@ -89,24 +80,28 @@ buttonDraw.addEventListener("click", function() {
   console.log(list);
 });
 
+let contador = -1;
+
 buttonSort.addEventListener("click", sorting);
 
 function sorting() {
   const bubbleSort = arr => {
-    let wall = arr.length - 1; //iniciamos el wall o muro al final del array
+    let wall = arr.length - 1; // Iniciamos el wall o muro al final del array
+
     while (wall > 0) {
       let index = 0;
       while (index < wall) {
-        //comparar las posiciones adyacentes, si la correcta es más grande, tenemos que intercambiar
+        // Comparar las posiciones adyacentes, si la correcta es más grande, tenemos que intercambiar
         if (arr[index][1] > arr[index + 1][1]) {
           let aux = arr[index];
           arr[index] = arr[index + 1];
           arr[index + 1] = aux;
-          cardsArrays(arr);
         }
         index++;
       }
-      wall--; //disminuir la pared para optimizar
+      wall--; // Disminuir la pared para optimizar
+      contador++;
+      cardsArrays(arr);
     }
     return arr;
   };
@@ -115,28 +110,61 @@ function sorting() {
 }
 
 function cardsArrays(arr) {
-  //let arraySort = sorting();
-  const boxCards = document.getElementById("boxCards");
-  let contador = 0;
+  const containerP = document.getElementById("containerP");
+
+  let serieContainer = document.createElement("div");
+  serieContainer.className = "serie-container";
+
+  let number = document.createElement("div");
+  number.className = "numero-serie";
+  number.innerHTML = `. ${contador}`;
+  serieContainer.appendChild(number);
+
+  let boxCards = document.createElement("div");
+  boxCards.className = "boxCards";
+
   arr.forEach(cardElement => {
+    const isRedSymbol = cardElement[0] === "♥" || cardElement[0] === "♦";
+    let cardNumberElement;
+    const isAsNumber = cardElement[1] === 1;
+
+    if (cardElement[1] === 1) {
+      cardNumberElement = cardElement[0];
+    } else if (cardElement[1] === 11) {
+      cardNumberElement = "J";
+    } else if (cardElement[1] === 12) {
+      cardNumberElement = "Q";
+    } else if (cardElement[1] === 13) {
+      cardNumberElement = "K";
+    } else {
+      cardNumberElement = cardElement[1];
+    }
+
     let newCard = document.createElement("div");
     newCard.className =
       "card d-flex p-0 m-0 justify-content-start align-items-center";
 
     newCard.innerHTML = `
-      <p>${contador}<p>
-        <div class="card-header d-flex justify-content-start align-items-center bg-transparent pt-3 ps-1">
-          <h2 class="symbol-header">${cardElement[0]}</h2>
-        </div>
-        <div class="card-body d-flex justify-content-center align-items-center">
-          <h1 class="number-body">${cardElement[1]}</h1>
-        </div>
-        <div class="card-footer d-flex justify-content-end align-items-center bg-transparent pb-2 pe-1">
-          <h2 class="symbol-footer" >${cardElement[0]}</h2>
-        </div>
-      `;
+      <div class="card-header d-flex justify-content-start align-items-center bg-transparent pt-3 ps-1">
+        <h2 class="symbol-header" style="color: ${
+          isRedSymbol ? "red" : "black"
+        }">${isAsNumber ? "" : cardElement[0]}</h2>
+      </div>
+      <div class="card-body d-flex justify-content-center align-items-center">
+        <h1 class="number-body" style="color: ${
+          isRedSymbol ? "red" : "black"
+        }">${cardNumberElement}</h1>
+      </div>
+      <div class="card-footer d-flex justify-content-end align-items-center bg-transparent pb-2 pe-1">
+        <h2 class="symbol-footer" style="color: ${
+          isRedSymbol ? "red" : "black"
+        }">${isAsNumber ? "" : cardElement[0]}</h2>
+      </div>
+    `;
 
     boxCards.appendChild(newCard);
-    contador++;
   });
+
+  serieContainer.appendChild(boxCards);
+  containerP.appendChild(serieContainer);
 }
